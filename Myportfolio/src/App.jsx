8bc4f71ed7e9822f71ee6a37dbdc1b6f00138f4d
Samiksha Verma <samiksha3ks@gmail.com>
2026-01-10 22:17:36 +0530

    Day 1: Project setup and base component structure

---
+++ Myportfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import { Route, Router } from 'react-router-dom'
+import Footer from './Components/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar/>
+      <Router>
+        <Route>
+          
+        </Route>
+      </Router>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
